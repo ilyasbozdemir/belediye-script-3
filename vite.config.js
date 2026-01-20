71569a5be12150fd9b297c0418efc7ebc60257a6
İlyas Bozdemir <bozdemir.ib70@gmail.com>
2026-01-20 12:48:40 +0300

    added

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '127.0.0.1',
-    proxy: {
-      '/api': {
-        target: 'https://localhost:7250',
-        changeOrigin: true,
-        secure: false, // For self-signed certificates in dev
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
+    host: '127.0.0.1'
   },
   build: {
-    ssr: true,
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        ssr: './src/entry-server.jsx'
-      }
-    }
+    outDir: 'dist'
   }
 })
